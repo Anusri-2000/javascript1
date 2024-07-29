2834e818aefed8e4581f874ee797357c3b5402dc
Anusri-2000 <141505912+Anusri-2000@users.noreply.github.com>
2024-07-29 22:10:09 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,60 @@
+alert("Hello Everyone");
+
+a="apple";
+console.log(a);
+
+var bunty="playing";
+console.log(bunty);
+
+var bunty="Singing";
+console.log(bunty);
+
+let school="nursery";
+console.log(school)
+
+school="primary";
+console.log(school);
+
+var a="this is text message";
+console.log(a);
+
+console.log(10,20,30);
+
+test=10>30;
+console.log(test);
+
+test=10<30;
+console.log(test);
+
+document.write("This is a Javascript text message");
+
+document.write("<h1>THIS IS HEADING TEXT</h1> <br>");
+
+document.write(11221144)
+
+let sampleview=null;
+console.log(sampleview);
+
+let apple="red";
+console.log(apple);
+
+let fruits=["apple","mango",55,{name:"Anu"}]
+console.log(fruits);
+
+const games={
+    cricket:"Dhoni",
+    football:"racer"
+}
+console.log(games);
+
+console.log(typeof games)
+ 
+console.log(typeof fruits);
+
+console.log(Array.isArray(fruits));
+
+
+
+
+
+
